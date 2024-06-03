f6fd03f1682639b474be39600880478bf336ab8d
Ljw <132702102+location132@users.noreply.github.com>
2024-06-04 00:42:53 +0900

    fix: 메인화면 완료

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,71 @@
+import 'dart:math' as math;
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _controller;
+  Animation<double>? _rotationAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2), // 애니메이션의 길이 설정
+      vsync: this,
+    );
+    _rotationAnimation =
+        Tween<double>(begin: 0, end: 2 * math.pi).animate(_controller!)
+          ..addListener(() {
+            setState(() {});
+          });
+
+    _controller!.repeat(); // 애니메이션을 반복 재생
+  }
+
+  @override
+  void dispose() {
+    _controller!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("컨테이너 회전 애니메이션"),
+      ),
+      body: Center(
+        child: Transform.rotate(
+          angle: _rotationAnimation!.value, // 회전 애니메이션 값 적용
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.blue,
+            child: Center(
+              child: Text(
+                '회전!',
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
