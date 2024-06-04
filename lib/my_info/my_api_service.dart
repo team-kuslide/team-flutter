c11ec2d26ec470447734951e4971463f3c0d7666
junhee <junhee4942@naver.com>
2024-06-05 02:46:17 +0900

    mypage

--- lib/my_info/my_api_service.dart
+++ lib/my_info/my_api_service.dart
@@ -4,12 +4,11 @@ import 'package:kuslide/my_info/api_model.dart';
 Future<List<MyPageResponse>?> fetchMyPageData() async {
   Dio dio = Dio();
 
-  String session = "N2FmOGI5MGQtMWIzMi00MTg2LWI1OGEtOTA2Zjc3N2E0MzEw";
+  String session = "";
   // 헤더에 세션 정보 추가
   dio.options.headers["Cookie"] = "SESSION=$session";
   try {
-    const url =
-        'http://kuslide-env.eba-w4k3vejk.ap-northeast-2.elasticbeanstalk.com/account/mypage'; // 실제 URL로 변경
+    const url = 'httage'; // 실제 URL로 변경
     Response response = await dio.get(url);
 
     if (response.statusCode == 200) {
