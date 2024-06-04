a8b0e840eb1b3fd8b72bf1a3c55125bf168b2e46
WhiteSna <tkdgns5674@gmail.com>
2024-06-05 04:21:36 +0900

    feat: 전공 드롭다운, 이메일dio

--- lib/coreService/routes.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:kuslide/Page/2_advanced_registration_page/2_advanced_registration_screen.dart';
-import 'package:kuslide/Page/3_main_Page/3_main_screen.dart';
-
-final Map<String, WidgetBuilder> appRoutes = {
-  '/AdvancedRegistrationPage': (context) =>
-      const AdvancedRegistrationPage(), // 회원가입 페이지
-  '/MainScreenPage': (context) => const MainScreenPage(), // 메인화면 페이지
-};
--- lib/coreService/verifty_form.dart
+++
@@ -1,5 +0,0 @@
-RegExp emailRegExp = RegExp(
-  r'^[a-zA-Z0-9._%+-]+@kku\.ac\.kr$',
-  caseSensitive: false,
-  multiLine: false,
-);
